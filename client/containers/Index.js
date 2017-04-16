358d209c88e528be44341cc5a8b70d2166ceb28f
ClemDelp <clement.delpuech@snips.ai>
2017-04-16 20:34:04 +0200

    add chat interface

--- client/containers/Index.js
+++ client/containers/Index.js
@@ -5,7 +5,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import GameCanvas from './GameCanvas'
-
+import Chat from '../components/Chat'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import Loading from '../components/Loading'
@@ -21,17 +21,17 @@ const Index = ({layers}) => {
   return (
     <MuiThemeProvider>
       <div>
-      <DialogBox />
-      {
-        Object.keys(layers).length > 0
-        ? <div className='row'>
-          <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
-            <GameCanvas />
+        <DialogBox />
+        {
+          Object.keys(layers).length > 0
+          ? <div className='row'>
+            <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+              <GameCanvas />
+              <Chat />
+            </div>
           </div>
-        </div>
-        : <Loading />
-
-      }
+          : <Loading />
+        }
       </div>
     </MuiThemeProvider>
   )
--- server/utils.js
+++ server/utils.js
@@ -1,14 +1,13 @@
 export function s4 () {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1)
 }
 
 export function guid () {
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4()
 }
 
-export function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+export function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
