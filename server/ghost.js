dad593f1365db24ae0c79bb6a014c5a303a0588c
ClemDelp <clement.delpuech@snips.ai>
2017-04-18 22:05:25 +0200

    clean ghost file

--- server/ghost.js
+++ server/ghost.js
@@ -1,36 +1,42 @@
-import {guid} from './utils'
-import {config} from '../config'
-import eden from 'node-eden'
+import Element from './Element'
 
-class Element {
-  constructor () {
-    this.name = eden.adam()
-    this.x = null
-    this.y = null
-    this.id = guid()
-  }
-  setPosition (position) {
-    this.x = position.x
-    this.y = position.y
-  }
-}
-
-export class Ghost extends Element {
+export default class Ghost extends Element {
   constructor () {
     super()
-    Object.keys(config.ghost).forEach((key) => {
-      this[key] = config.ghost[key]
-    })
-    this.deplacements = Object.keys(config.ghost.animations)
-  }
-}
-
-export class Player extends Element {
-  constructor () {
-    super()
-    Object.keys(config.player).forEach((key) => {
-      this[key] = config.player[key]
-    })
-    this.deplacements = Object.keys(config.player.animations)
+    this.type = 'ghost'
+    this.canHover = ['grass']
+    this.img = 'assets/sprites/ghosts.png'
+    this.animations = {
+      top: {
+        frames: [120, 140],
+        duration: 10,
+        loop: true
+      },
+      right: {
+        frames: [0, 20],
+        duration: 10,
+        loop: true
+      },
+      bottom: {
+        frames: [40, 60],
+        duration: 10,
+        loop: true
+      },
+      left: {
+        frames: [80, 100],
+        duration: 10,
+        loop: true
+      }
+    }
+    this.body.collideWorldBounds = false
+    this.size = [32, 32]
+    this.scale = [0.75, 0.75]
+    this.orientation = 'bottom'
+    this.velocity = 5 // px per move
+    this.speed = 100
+    this.triesBeforeExplosion = 2
+    this.explosion = true
+    this.tileName = 'ghost'
+    this.isoZ = 200
   }
 }
